75c90e458a5e49987110076971b36c91425c11e0
dsnunes07 <30977109+dsnunes07@users.noreply.github.com>
2020-01-24 16:00:42 -0300

    Update dev track (#7)
    * add icons
    
    * update track
    
    * change mac0475 icon

--- src/tracks/sistemas.js
+++ src/tracks/sistemas.js
@@ -1,18 +1,24 @@
 import { BoxTypes } from '../definitions/constants'
+import { validateBox } from '../definitions/validation'
 
 export default {
+  description:
+    'O aluno deverá completar o número de disciplinas necessário por bloco (indicado abaixo) + 2 disciplinas quaisquer dentro de qualquer um dos módulos.',
   boxes: {
     left: [
       {
-        title: 'Desenvolvimento de Software I',
-        classes: ['MAC0218'],
-        type: BoxTypes.COMPLETE_ALL,
-      },
-      {
-        title: 'Desenvolvimento de Software II',
-        classes: ['MAC0332', 'MAC0413', 'MAC0467', 'MAC0470', 'MAC0472'],
+        title: 'Desenvolvimento de Software',
+        classes: ['MAC0218',
+                  'MAC0332',
+                  'MAC0346',
+                  'MAC0413',
+                  'MAC0467',
+                  'MAC0470', 
+                  'MAC0472',
+                  'MAC0475',
+                ],
         type: BoxTypes.COMPLETE_SOME,
-        minimum: 3,
+        minimum: 2,
       },
     ],
     right: [
@@ -24,17 +30,36 @@ export default {
           'MAC0352',
           'MAC0463',
           'MAC0469',
-          'MAC0471',
         ],
-        type: BoxTypes.COMPLETE_SOME,
-        minimum: 3,
-      },
-      {
-        title: 'Bancos de Dados',
-        classes: ['MAC0426', 'MAC0439', 'MAC0459'],
         type: BoxTypes.COMPLETE_SOME,
         minimum: 2,
       },
+      {
+        title: 'Bancos de Dados',
+        classes: ['MAC0426',
+                  'MAC0439',
+                  'MAC0459',
+                 ],
+        type: BoxTypes.COMPLETE_SOME,
+        minimum: 1,
+      },
     ],
+  },
+  validate: (boxes, doneClasses) => {
+    const trackClasses = [...boxes.left, ...boxes.right].reduce(
+      (acc, cur) => [...acc, ...cur.classes],
+      []
+    )
+    const completedTrackClasses = trackClasses.reduce(
+      (acc, cur) => (doneClasses.includes(cur) ? acc + 1 : acc),
+      0
+    )
+    const basicModules = [boxes.left[0], boxes.right[0], boxes.right[1]]
+    const completedBasicModules = basicModules.reduce(
+      (acc, cur) => (validateBox(cur, doneClasses) ? acc + 1 : acc),
+      0
+    )
+
+    return completedTrackClasses >= 7 && completedBasicModules == 3
   },
 }
